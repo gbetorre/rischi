aa4dfd41b38e43a98b625bcbab8ea5340ee3ee7f
gbetorre <gianroberto.torre@gmail.com>
2022-09-27 15:48:40 +0200

    Aggiunto metodo per la generazione del json; ottimizzazioni codice

--- src/it/rol/command/ProcessCommand.java
+++ src/it/rol/command/ProcessCommand.java
@@ -1,11 +1,13 @@
 /*
- *   Process Mapping Software: Modulo Applicazione web per la visualizzazione
- *   delle schede di indagine su allocazione risorse dell'ateneo,
- *   per la gestione dei processi on line (pms).
+ *   Risk Mapping Software: Applicazione web per la gestione di 
+ *   sondaggi inerenti al rischio corruttivo cui i processi organizzativi
+ *   dell'ateneo possono essere esposti e per la gestione di reportistica
+ *   e mappature per la gestione dei "rischi on line" (rol).
  *
- *   Process Mapping Software (pms)
- *   web applications to publish, and manage,
- *   processes, assessment and skill information.
+ *   Risk Mapping Software (rms)
+ *   web applications to make survey about the amount and kind of risk
+ *   which each process is exposed, and to publish, and manage,
+ *   report and risk information.
  *   Copyright (C) renewed 2022 Giovanroberto Torre
  *   all right reserved
  *
@@ -31,6 +33,9 @@
 
 package it.rol.command;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -49,7 +54,9 @@ import com.oreilly.servlet.ParameterParser;
 import it.rol.ConfigManager;
 import it.rol.Constants;
 import it.rol.DBWrapper;
+import it.rol.Data;
 import it.rol.Main;
+import it.rol.Utils;
 import it.rol.bean.CodeBean;
 import it.rol.bean.ItemBean;
 import it.rol.bean.PersonBean;
@@ -61,7 +68,8 @@ import it.rol.exception.WebStorageException;
 
 /**
  * <p><code>ProcessCommand.java</code><br />
- * Implementa la logica per la gestione dei processi on line (PROL).</p>
+ * Implementa la logica per la gestione dei processi censiti dall'anticorruzione
+ * ai fini della valutazione del rischio corruttivo, on line (ROL).</p>
  *
  * @author <a href="mailto:gianroberto.torre@gmail.com">Giovanroberto Torre</a>
  */
@@ -88,13 +96,13 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
      */
     private static final String nomeFileElenco = "/jsp/prElenco.jsp";
     /**
-     * Pagina a cui la command reindirizza per mostrare i macroprocessi con vista alternativa
+     * Nome del file json della Command (dipende dalla pagina di default)
      */
-    private static final String nomeFileElencoAlt = "/jsp/prElencoDC.jsp";
+    private String nomeFileJson = nomeFileElenco.substring(nomeFileElenco.lastIndexOf('/'), nomeFileElenco.indexOf(DOT));
     /**
      * Struttura contenente le pagina a cui la command fa riferimento per mostrare tutti gli attributi del progetto
      */
-    private static final HashMap<String, String> nomeFile = new HashMap<String, String>();
+    //private static final HashMap<String, String> nomeFile = new HashMap<>();
     /**
      *  Processo di dato id
      */
@@ -105,7 +113,7 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
      * Crea una nuova istanza di ProcessCommand
      */
     public ProcessCommand() {
-        /*;*/   // It Doesn't Anything
+        /*;*/   // It doesn't anything
     }
 
 
@@ -129,7 +137,6 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
           throw new CommandException(msg);
         }
         // Carica la hashmap contenente le pagine da includere in funzione dei parametri sulla querystring
-        nomeFile.put(PART_MACROPROCESS, nomeFileElencoAlt);
         //nomeFile.put(Query.PART_PROJECT, this.getPaginaJsp());
     }
 
@@ -157,14 +164,15 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
         // Recupera o inizializza 'codice rilevazione' (Survey)
         String codeSur = parser.getStringParameter("r", DASH);
         // Recupera o inizializza 'id processo'
-        int idPr = parser.getIntParameter("id", -1);
+        //int idPr = parser.getIntParameter("id", -1);
         // Recupera o inizializza 'tipo pagina'
-        String part = parser.getStringParameter("p", DASH);
-        String pr = null;
+        //String part = parser.getStringParameter("p", DASH);
         // Dichiara la pagina a cui reindirizzare
         String fileJspT = null;
         // Dichiara elenco di processi
-        AbstractList<ProcessBean> m = new ArrayList<ProcessBean>();
+        AbstractList<ProcessBean> macrosat = new ArrayList<>();
+        // Prepara un oggetto contenente i parametri opzionali per i nodi
+        ItemBean options = new ItemBean("#009966",  VOID_STRING,  VOID_STRING, VOID_STRING, PRO_PFX, NOTHING);
         /* ******************************************************************** *
          *      Instanzia nuova classe WebStorage per il recupero dei dati      *
          * ******************************************************************** */
@@ -205,24 +213,15 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
          * ******************************************************************** */
         // Decide il valore della pagina
         try {
-            // Il parametro di navigazione 'rilevazione' Ã¨ obbligatorio
+            // Il parametro di navigazione 'rilevazione' è obbligatorio
             if (!codeSur.equals(DASH)) {
-                // Estrae il set di macroprocessi
-                m = retrieveMacroBySurvey(user, codeSur, db);
-                // Il parametro di navigazione 'p' permette di addentrarsi nelle funzioni
-                if (nomeFile.containsKey(part)) {
-                    // Viene richiesta la visualizzazione alternativa dei macroprocessi
-                    if (part.equals(PART_MACROPROCESS)) {
-                        fileJspT = nomeFile.get(part);
-                    }
-                } else {
-                    // Viene richiesta la visualizzazione di un elenco di macroprocessi
-                    m = retrieveMacroBySurvey(user, codeSur, db);
-                    // Recupera o inizializza il codice dell'eventuale (macro)processo che verrÃ  passato alla vista
-                    pr = parser.getStringParameter("pr", DASH);
-                    fileJspT = nomeFileElenco;
-                }
-            } else {    // Manca il codice rilevazione
+                // Viene richiesta la visualizzazione di un elenco di macroprocessi
+                macrosat = retrieveMacroAtBySurvey(user, codeSur, db);
+                // Genera il file json contenente le informazioni strutturate
+                printJson(req, macrosat, nomeFileJson, options);
+                // Imposta la jsp
+                fileJspT = nomeFileElenco;
+            } else {    // Manca il codice rilevazione!!
                 String msg = FOR_NAME + "Impossibile recuperare il codice della rilevazione.\n";
                 LOG.severe(msg + "Qualcuno ha probabilmente alterato il codice rilevazione nell\'URL della pagina.\n");
                 throw new CommandException(msg);
@@ -244,19 +243,18 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
          *              Settaggi in request dei valori calcolati                *
          * ******************************************************************** */
         // Imposta nella request elenco macroprocessi
-        req.setAttribute("macroprocessi", m);
-        // Imposta nella request codice del (macro)processo
-        if (pr != null)
-            req.setAttribute("pr", pr);
+        //req.setAttribute("macroprocessi", macrosat);
         // Imposta la Pagina JSP di forwarding
         req.setAttribute("fileJsp", fileJspT);
     }
 
     
     /**
-     * <p>Restituisce un ArrayList di tutti macroprocessi censiti 
-     * dall'anticorruzionetrovati in base a una rilevazione il cui 
-     * identificativo viene passato come argomento.</p>
+     * <p>Restituisce un ArrayList (albero, vista gerarchica) 
+     * di tutti macroprocessi censiti dall'anticorruzione 
+     * trovati in base a una rilevazione il cui identificativo 
+     * viene passato come argomento; ogni macroprocesso contiene 
+     * internamente i suoi processi e questi gli eventuali sottoprocessi.</p>
      *
      * @param codeSurvey    il codice della rilevazione
      * @param user          utente loggato; viene passato ai metodi del DBWrapper per controllare che abbia i diritti di fare quello che vuol fare
@@ -290,8 +288,8 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
 
 
     /**
-     * <p>Restituisce un ArrayList di tutti processi/macroprocessi trovati in base a
-     * una rilevazione il cui identificativo viene passato come argomento.</p>
+     * <p>Restituisce un ArrayList di tutti processi/macroprocessi trovati in base 
+     * a una rilevazione, il cui identificativo viene passato come argomento.</p>
      *
      * @param codeSurvey    il codice della rilevazione
      * @param user          utente loggato; viene passato ai metodi del DBWrapper per controllare che abbia i diritti di fare quello che vuol fare
@@ -299,10 +297,10 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
      * @return <code>ArrayList&lt;ProcessBean&gt;</code> - lista di macroprocessi recuperati
      * @throws CommandException se si verifica un problema nell'estrazione dei dati, o in qualche tipo di puntamento
      */
-    public static ArrayList<ProcessBean> retrieveMacroBySurvey(PersonBean user,
-                                                               String codeSurvey,
-                                                               DBWrapper db)
-                                                        throws CommandException {
+    private static ArrayList<ProcessBean> retrieveMacroBySurvey(PersonBean user,
+                                                                String codeSurvey,
+                                                                DBWrapper db)
+                                                         throws CommandException {
         ArrayList<ProcessBean> macro = null;
         try {
             // Estrae i macroprocessi in una data rilevazione
@@ -341,12 +339,12 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
      * @throws CommandException se si verifica un problema nell'estrazione dei dati, o in qualche tipo di puntamento
      * @see it.rol.DBWrapper#getMacroByStruct(it.rol.bean.PersonBean, int, byte, it.rol.bean.CodeBean)
      */
-    public static ArrayList<ProcessBean> retrieveMacroByStruct(PersonBean user,
-                                                                 int idS,
-                                                                 byte level,
-                                                                 String codeSurvey,
-                                                                 DBWrapper db)
-                                                        throws CommandException {
+    private static ArrayList<ProcessBean> retrieveMacroByStruct(PersonBean user,
+                                                                int idS,
+                                                                byte level,
+                                                                String codeSurvey,
+                                                                DBWrapper db)
+                                                         throws CommandException {
         ArrayList<ProcessBean> mP = null;
         CodeBean survey = ConfigManager.getSurvey(codeSurvey);
         try {
@@ -383,7 +381,7 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
      */
     public static LinkedHashMap<Integer, ProcessBean> decant(Vector<ProcessBean> processes)
                                                       throws CommandException {
-        LinkedHashMap<Integer, ProcessBean> userProcesses = new LinkedHashMap<Integer, ProcessBean>(7);
+        LinkedHashMap<Integer, ProcessBean> userProcesses = new LinkedHashMap<>(7);
         for (int i = 0; i < processes.size(); i++) {
             try {
                 ProcessBean process = processes.elementAt(i);
@@ -466,6 +464,126 @@ public class ProcessCommand extends ItemBean implements Command, Constants {
             String msg = FOR_NAME + "Si e\' verificato un problema nel calcolo di un boolean da un Dictionary.\n" + e.getMessage();
             LOG.severe(msg);
             throw new CommandException(msg, e);
+        }
+    }
+    
+
+    /**
+     * <p>Gestisce la creazione di un file JSON formattato per la libreria
+     * orgchart.js (versione 1.0.5) contenente le informazioni relative alla
+     * gerarchia dei macroprocessi censiti dall'anticorruzione.</p>
+     * 
+     * @param req HttpServletRequest    per recuperare la path assoluta della web application
+     * @param proc                      lista gerarchica dei processi da rappresentare
+     * @param filename                  nome del file json da generare, parametrizzato
+     * @param options                   opzioni di formattazione aggiuntive
+     * @throws CommandException         se si verifica un puntamento a null o in genere nei casi in cui nella gestione del flusso informativo di questo metodo si verifica un problema
+     */
+    public static void printJson(HttpServletRequest req,
+                                 AbstractList<ProcessBean> proc,
+                                 String filename,
+                                 ItemBean options)
+                          throws CommandException {
+        FileWriter out = null;
+        try {
+            /* **************************************************************** *
+             *     Controlla che la directory esista; se non esiste, la crea   *
+             * **************************************************************** */
+            // Ottiene la path assoluta della root della web application (/)
+            String appPath = req.getServletContext().getRealPath(VOID_STRING);
+            LOG.info(FOR_NAME + "appPath vale: " + appPath);
+            // Crea la documents root directory (/documenti) se essa non esiste
+            String documentsRootName = appPath + ConfigManager.getDirDocuments();
+            File documentsRoot = new File(documentsRootName);
+            if (!documentsRoot.exists()) {
+                if (documentsRoot.mkdir()) {
+                    LOG.info(FOR_NAME + documentsRootName + " creata.\n");
+                } else {
+                    String msg = FOR_NAME + "Attenzione: impossibile creare la directory \'documenti\' sotto " + documentsRootName;
+                    LOG.severe(msg);
+                    // Inutile continuare
+                    throw new CommandException(msg + " Verificare eventualmente di avere i diritti di scrittura nella root dell\'applicazione.\n");
+                }
+            }
+            // Crea la sottodirectory dei file json (/documenti/json) se non esiste
+            File jsonDir = new File(appPath + ConfigManager.getDirJson());
+            if (!jsonDir.exists()) {
+                jsonDir.mkdir();
+                LOG.info(FOR_NAME + jsonDir + " creata.\n");
+            }
+            /* ************************************************************ *
+             *                    Genera e scrive il json                   *
+             * ************************************************************ */
+            String givenName = filename;
+            LOG.info(FOR_NAME + "Nome stabilito per il file: " + givenName);
+            String extension = DOT + JSON;
+            // Crea il FileWriter (il nome della variabile è quello standard del PrintWriter, finesse...)
+            out = new FileWriter(jsonDir + File.separator + givenName + extension);
+            // Json begins
+            StringBuilder json = new StringBuilder("[");
+            // First Node: the principal
+            json.append(Data.getStructureJsonNode(COMMAND_PROCESS, "1000", null, "Processi Anticorruttivi", VOID_STRING, null, PRO_PFX, NOTHING)).append(",\n");
+            int count = NOTHING;
+            do {
+                ProcessBean p1 = proc.get(count);
+                // MACROPROCESSI
+                json.append(Data.getStructureJsonNode(COMMAND_PROCESS, 
+                                                      p1.getTag(), 
+                                                      "1000", 
+                                                      Utils.checkQuote(p1.getNome()), 
+                                                      VOID_STRING, 
+                                                      options.getNome(), options.getIcona(), 
+                                                      p1.getLivello())).append(",\n");
+                // PROCESSI
+                for (ProcessBean p2 : p1.getProcessi()) {
+                    json.append(Data.getStructureJsonNode(COMMAND_PROCESS, 
+                                                          p2.getTag(), 
+                                                          p1.getTag(), 
+                                                          Utils.checkQuote(p2.getNome()), 
+                                                          VOID_STRING, 
+                                                          options.getNome(), options.getIcona(),
+                                                          p2.getLivello())).append(",\n");
+                    // SOTTOPROCESSI
+                    for (ProcessBean p3 : p2.getProcessi()) {
+                        json.append(Data.getStructureJsonNode(COMMAND_PROCESS, 
+                                                              p3.getTag(), 
+                                                              p2.getTag(), 
+                                                              Utils.checkQuote(p3.getNome()), 
+                                                              VOID_STRING, 
+                                                              options.getNome(), options.getIcona(),
+                                                              p3.getLivello())).append(",\n");
+                    }
+                }
+                count++;
+            } while (count < proc.size());
+            json = new StringBuilder(json.substring(NOTHING, json.length() - 2));
+            json.append("]");
+            out.write(json.toString());
+            LOG.info(FOR_NAME + "Salvataggio in file system effettuato.\n");
+        } catch (IOException ioe) {
+            throw new CommandException("Impossibile scrivere il file. Verificare nome e percorso.\n" + ioe.getMessage(), ioe);
+        } catch (IllegalStateException ise) {
+            throw new CommandException("Impossibile redirigere l'output. Verificare se la risposta e\' stata gia\' committata.\n" + ise.getMessage(), ise);
+        } catch (NullPointerException npe) {
+            throw new CommandException("Errore nell'estrazione dei dipartimenti che gestiscono il corso.\n" + npe.getMessage(), npe);
+        } catch (Exception e) {
+            String msg = FOR_NAME + "Si e\' verificato un problema.\n";
+            LOG.severe(msg);
+            throw new CommandException(msg + e.getMessage(), e);
+        // Tenta in ogni caso di chiudere il file
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                    LOG.info("File chiuso con successo.");
+                } catch (IOException ioe) {
+                    String msg = FOR_NAME + "Ooops... problema nella chiusura del file!\n";
+                    LOG.severe(msg);
+                    throw new CommandException(msg + ioe.getMessage());
+                } catch (Exception e) {
+                    throw new CommandException(FOR_NAME + e.getMessage());
+                }
+            }
         }
     }
 
