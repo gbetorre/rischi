79d922ac028e24db316584f8f2aad6a4527fb89e
gbetorre <gianroberto.torre@gmail.com>
2023-01-30 11:24:52 +0100

    Eliminato riferimento all'interfaccia Serializable, già presente nel padre

--- src/it/rol/bean/RiskBean.java
+++ src/it/rol/bean/RiskBean.java
@@ -1,34 +1,31 @@
 /*
- *   Alma on Line: Applicazione WEB per la visualizzazione 
- *   delle schede di indagine su popolazione dell'ateneo,
- *   della gestione dei progetti on line (POL) 
- *   e della preparazione e del monitoraggio delle informazioni riguardanti 
- *   l'offerta formativa che hanno ricadute sulla valutazione della didattica 
- *   (questionari on line - QOL).
- *   
- *   Copyright (C) 2018 Giovanroberto Torre<br />
- *   Alma on Line (aol), Projects on Line (pol), Questionnaire on Line (qol);
- *   web applications to publish, and manage, students evaluation,
- *   projects, students and degrees information.
- *   Copyright (C) renewed 2018 Universita' degli Studi di Verona, 
+ *   Risk Mapping Software: Applicazione web per la gestione di 
+ *   sondaggi inerenti al rischio corruttivo cui i processi organizzativi
+ *   dell'ateneo possono essere esposti e per la gestione di reportistica
+ *   e mappature per la gestione dei "rischi on line" (rol).
+ *
+ *   Risk Mapping Software (rms)
+ *   web applications to make survey about the amount and kind of risk
+ *   which each process is exposed, and to publish, and manage,
+ *   report and risk information.
+ *   Copyright (C) renewed 2023 Giovanroberto Torre
  *   all right reserved
  *
- *   This program is free software; you can redistribute it and/or modify 
- *   it under the terms of the GNU General Public License as published by 
- *   the Free Software Foundation; either version 2 of the License, or 
- *   (at your option) any later version. 
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
  *
- *   This program is distributed in the hope that it will be useful, 
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
- *   GNU General Public License for more details. 
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License 
- *   along with this program; if not, write to the Free Software 
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
  *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA<br>
- *   
- *   Giovanroberto Torre <giovanroberto.torre@univr.it>
- *   Sistemi Informatici per il Reporting di Ateneo
+ *
+ *   Giovanroberto Torre <gianroberto.torre@gmail.com>
  *   Universita' degli Studi di Verona
  *   Via Dell'Artigliere, 8
  *   37129 Verona (Italy)
@@ -36,17 +33,14 @@
 
 package it.rol.bean;
 
-import java.io.Serializable;
-
-import it.rol.Query;
 import it.rol.exception.AttributoNonValorizzatoException;
 
 /**
  * <p>Classe usata per rappresentare un rischio.</p>
  * 
- * @author <a href="mailto:andrea.tonel@studenti.univr.it">Andrea Tonel</a>
+ * @author <a href="mailto:giovanroberto.torre@univr.it">Giovanroberto Torre</a>
  */
-public class RiskBean extends CodeBean implements Serializable {
+public class RiskBean extends CodeBean {
 
 	/**
 	 * La serializzazione necessita di dichiarare una costante di tipo long
@@ -89,7 +83,7 @@ public class RiskBean extends CodeBean implements Serializable {
 	/**
 	 * Restituisce l'impatto del rischio
 	 * @return <code>impatto</code> - impatto del rischio
-	 * @throws it.alma.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e impatto non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
+	 * @throws it.rol.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e impatto non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
 	 * oppure impatto non &egrave; stato valorizzato correttamente
 	 */
 	public String getImpatto() throws AttributoNonValorizzatoException {
@@ -119,7 +113,7 @@ public class RiskBean extends CodeBean implements Serializable {
 	/**
 	 * Restituisce il livello del rischio
 	 * @return <code>livello</code> - livello del rischio
-	 * @throws it.alma.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e livello non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
+	 * @throws it.rol.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e livello non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
 	 * oppure livello non &egrave; stato valorizzato correttamente
 	 */
 	public String getLivello() throws AttributoNonValorizzatoException {
@@ -149,7 +143,7 @@ public class RiskBean extends CodeBean implements Serializable {
 	/**
 	 * Restituisce lo stato del rischio
 	 * @return <code>stato</code> - stato del rischio
-	 * @throws it.alma.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e stato non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
+	 * @throws it.rol.exception.AttributoNonValorizzatoException  eccezione che viene sollevata se questo oggetto viene usato e stato non &egrave; stato valorizzato (&egrave; un dato obbligatorio) 
 	 * oppure stato non &egrave; stato valorizzato correttamente
 	 */
 	public String getStato() throws AttributoNonValorizzatoException {
