016b6d1eff6c4fb973a5a0fdf194d7d518465f40
gbetorre <gianroberto.torre@gmail.com>
2023-10-02 12:15:30 +0200

    Rimosso codice legacy

--- src/it/rol/command/ReportCommand.java
+++ src/it/rol/command/ReportCommand.java
@@ -8,7 +8,7 @@
  *   web applications to make survey about the amount and kind of risk
  *   which each process is exposed, and to publish, and manage,
  *   report and risk information.
- *   Copyright (C) renewed 2022 Giovanroberto Torre
+ *   Copyright (C) 2022 renewed 2023 Giovanroberto Torre
  *   all right reserved
  *
  *   This program is free software; you can redistribute it and/or modify
@@ -93,7 +93,7 @@ public class ReportCommand extends ItemBean implements Command, Constants {
     /**
      * Struttura contenente le pagina a cui la command fa riferimento per mostrare tutti gli attributi del progetto
      */
-    private static final HashMap<String, String> nomeFile = new HashMap<String, String>();
+    private static final HashMap<String, String> nomeFile = new HashMap<>();
     /**
      *  Processo di dato id
      */
@@ -161,16 +161,6 @@ public class ReportCommand extends ItemBean implements Command, Constants {
         String part = parser.getStringParameter("p", DASH);
         // Dichiara la pagina a cui reindirizzare
         String fileJspT = null;
-        // Dichiara elenco di aree funzionali
-        AbstractList<ItemBean> aree = new ArrayList<ItemBean>();
-        // Dichiara elenco di ruoli giuridici
-        AbstractList<ItemBean> qualifiche = new ArrayList<ItemBean>();
-        // Dichiara elenco di persone in base a specifica ricerca
-        AbstractList<PersonBean> people = null;
-        // Dichiara persona
-        PersonBean person = null;
-        // Dichiara elenco di macroprocessi in base a specifica persona
-        AbstractList<ProcessBean> mp = null;
         // Dichiara mappa di parametri di ricerca
         HashMap<String, LinkedHashMap<String, String>> params = null;
         // Preprara BreadCrumbs
@@ -214,17 +204,14 @@ public class ReportCommand extends ItemBean implements Command, Constants {
          *                   Decide il valore della pagina                      *
          * ******************************************************************** */
         try {
-            // Il parametro di navigazione 'rilevazione' Ã¨ obbligatorio
+            // Il parametro di navigazione 'rilevazione' è obbligatorio
             if (!codeSur.equals(DASH)) {
-                // Recupero delle liste comuni
-                aree = db.getAreeFunzionali(user);
-                qualifiche = db.getRuoliGiuridici(user);
                 // Il parametro di navigazione 'p' permette di addentrarsi nelle funzioni
                 if (nomeFile.containsKey(part)) {
                     // Imposta il valore della pagina di ricerca
                     fileJspT = nomeFile.get(part);
                 } else {
-                    // Viene richiesta la visualizzazione della pagina di ricerca
+                    // Viene richiesta la visualizzazione della pagina di report
                     fileJspT = nomeFileElenco;
                 }
             } else {    // manca il codice rilevazione
@@ -232,10 +219,6 @@ public class ReportCommand extends ItemBean implements Command, Constants {
                 LOG.severe(msg + "Qualcuno ha probabilmente alterato il codice rilevazione nell\'URI della pagina.\n");
                 throw new CommandException(msg);
             }
-        } catch (WebStorageException wse) {
-            String msg = FOR_NAME + "Si e\' verificato un problema nel recupero di valori dal db.\n";
-            LOG.severe(msg);
-            throw new CommandException(msg + wse.getMessage(), wse);
         } catch (NullPointerException npe) {
             String msg = FOR_NAME + "Si e\' verificato un problema di puntamento a null.\n";
             LOG.severe(msg);
@@ -248,25 +231,9 @@ public class ReportCommand extends ItemBean implements Command, Constants {
         /* ******************************************************************** *
          *              Settaggi in request dei valori calcolati                *
          * ******************************************************************** */
-        // Imposta nella request elenco aree funzionali
-        req.setAttribute("areeFunz", aree);
-        // Imposta nella request elenco ruoli giuridici
-        req.setAttribute("ruoliGiur", qualifiche);
-        // Imposta nella request elenco persone trovate, nel caso in cui siano valorizzate
-        if (people != null) {
-            req.setAttribute("persone", people);
-        }
         // Imposta nella request le chiavi di ricerca, se presenti
         if (params != null) {
             req.setAttribute("tokens", params);
-        }
-        // Imposta nella request singola persona, nel caso in cui sia stata richiesta
-        if (person != null) {
-            req.setAttribute("persona", person);
-        }
-        // Imposta nella request lista macro/processi su cui Ã¨ allocata la persona, in caso esistano
-        if (mp != null) {
-            req.setAttribute("macroprocessi", mp);
         }
         // Imposta nella request breadcrumbs in caso siano state personalizzate
         if (bC != null) {
@@ -275,96 +242,6 @@ public class ReportCommand extends ItemBean implements Command, Constants {
         }
         // Imposta la Pagina JSP di forwarding
         req.setAttribute("fileJsp", fileJspT);
-    }
-
-
-    /**
-     * <p>Restituisce un Vector di tutte le persone allocate su macro/processo
-     * organizzativo il cui identificativo viene passato come parametro; 
-     * ciascuna delle persone contiene al proprio interno specifici attributi
-     * caratterizzanti l'associazione della persona alla propria struttura.</p>
-     *
-     * @param type          identifica se la richiesta riguarda un macroprocesso o un processo
-     * @param id            l'identificativo del macroprocesso o processo
-     * @param codeSurvey    il codice della rilevazione
-     * @param user          utente loggato; viene passato ai metodi del DBWrapper per controllare che abbia i diritti di fare quello che vuol fare
-     * @param db            WebStorage per l'accesso ai dati
-     * @return <code>ArrayList&lt;PersonBean&gt;</code> - lista di persone recuperate
-     * @throws CommandException se si verifica un problema nell'estrazione dei dati, o in qualche tipo di puntamento
-     */
-    public static ArrayList<PersonBean> retrievePeopleByMacroOrProcess(String type,
-                                                                       int id,
-                                                                       String codeSurvey,
-                                                                       PersonBean user,
-                                                                       DBWrapper db)
-                                                                throws CommandException {
-        ArrayList<PersonBean> people = null;
-        CodeBean survey = ConfigManager.getSurvey(codeSurvey);
-        try {
-            // Estrae le persone allocate su un dato macroprocesso in una data rilevazione
-            people = db.getPeopleByMacroOrProcess(user, type, id, survey.getId());
-        } catch (AttributoNonValorizzatoException anve) {
-            String msg = FOR_NAME + "Identificativo della rilevazione non recuperabile; problema nel metodo di estrazione delle persone per macro/processi.\n";
-            LOG.severe(msg);
-            throw new CommandException(msg + anve.getMessage(), anve);
-        } catch (WebStorageException wse) {
-            String msg = FOR_NAME + "Si e\' verificato un problema nel recupero delle persone su macro/processo.\n";
-            LOG.severe(msg);
-            throw new CommandException(msg + wse.getMessage(), wse);
-        } catch (NullPointerException npe) {
-            String msg = FOR_NAME + "Si e\' verificato un problema di puntamento a null.\n Attenzione: controllare di essere autenticati nell\'applicazione!\n";
-            LOG.severe(msg);
-            throw new CommandException(msg + npe.getMessage(), npe);
-        } catch (Exception e) {
-            String msg = FOR_NAME + "Si e\' verificato un problema.\n";
-            LOG.severe(msg);
-            throw new CommandException(msg + e.getMessage(), e);
-        }
-        return people;
-    }
-
-
-    /**
-     * <p>Restituisce un Vector di tutte le persone allocate su un macroprocesso
-     * organizzativo il cui identificativo viene passato come parametro; 
-     * ciascuna delle persone contiene al proprio interno specifici attributi
-     * caratterizzanti l'associazione della persona alla propria struttura.</p>
-     *
-     * @param idMacro       l'identificativo del macroprocesso
-     * @param codeSurvey    il codice della rilevazione
-     * @param user          utente loggato; viene passato ai metodi del DBWrapper per controllare che abbia i diritti di fare quello che vuol fare
-     * @param db            WebStorage per l'accesso ai dati
-     * @return <code>ArrayList&lt;PersonBean&gt;</code> - lista di persone recuperate
-     * @throws CommandException se si verifica un problema nell'estrazione dei dati, o in qualche tipo di puntamento
-     */
-    public static ArrayList<PersonBean> retrievePeopleByMacro(int idMacro,
-                                                              String codeSurvey,
-                                                              PersonBean user,
-                                                              DBWrapper db)
-                                                       throws CommandException {
-        return retrievePeopleByMacroOrProcess(PART_MACROPROCESS, idMacro, codeSurvey, user, db);
-    }
-
-
-    /**
-     * <p>Restituisce un Vector di tutte le persone allocate su un processo
-     * organizzativo il cui identificativo viene passato come parametro; 
-     * ciascuna delle persone contiene al proprio interno specifici attributi
-     * caratterizzanti l'associazione della persona alla propria struttura.</p>
-     *
-     * @param idProcess     l'identificativo del processo
-     * @param codeSurvey    il codice della rilevazione
-     * @param user          utente loggato; viene passato ai metodi del DBWrapper per controllare che abbia i diritti di fare quello che vuol fare
-     * @param db            WebStorage per l'accesso ai dati
-     * @return <code>ArrayList&lt;PersonBean&gt;</code> - lista di persone recuperate
-     * @throws CommandException se si verifica un problema nell'estrazione dei dati, o in qualche tipo di puntamento
-     */
-    public static ArrayList<PersonBean> retrievePeopleByProcess(int idProcess,
-                                                                String codeSurvey,
-                                                                PersonBean user,
-                                                                DBWrapper db)
-                                                         throws CommandException {
-        return retrievePeopleByMacroOrProcess(PART_PROCESS, idProcess, codeSurvey, user, db);
     }
 
 }
