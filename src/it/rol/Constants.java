996f3506fd2c273784f614e83a5c2ed3ec7d8c2c
gbetorre <gianroberto.torre@gmail.com>
2024-02-26 10:50:02 +0100

    Rinominato prefisso nome del file di report 'strutture e rischi' in formato RTF

--- src/it/rol/Constants.java
+++ src/it/rol/Constants.java
@@ -538,7 +538,7 @@ public interface Constants extends Serializable {
     /**
      * Costante per i nomi di estrazioni dati "tabella MDM"
      */
-    public static final String REPORT_MDM = "tabella_mdm";
+    public static final String REPORT_MDM = "tabella_rischi";
     /**
      * Costante per il log delle differenze tra i valori indicatori in cache e quelli in memoria
      */
