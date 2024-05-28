b6156f48143b68a16732377dfb7db229779d1f5d
gbetorre <gianroberto.torre@gmail.com>
2024-05-28 10:46:46 +0200

    Spostata dal DBWrapper implementazione di metodi dichiarati nell'interfaccia Query

--- src/it/rol/QueryImpl.java
+++ src/it/rol/QueryImpl.java
@@ -14,19 +14,117 @@ import it.rol.exception.WebStorageException;
  * @author outer-root
  *
  */
-public class QueryImpl implements Query {
+public class QueryImpl implements Query, Constants {
 
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 7339151352217708748L;
+
+
+    /* (non-Javadoc)
+     * @see it.rol.Query#getQueryStructures(int, int, int, int, int)
+     */
+    @SuppressWarnings("javadoc")
     @Override
     public String getQueryStructures(int idR, int idl4, int idl3, int idl2, int idl1) {
-        // TODO Auto-generated method stub
-        return null;
+        String tableFrom = null;
+        String tableWhere = null;
+        byte level = (byte) DEFAULT_ID;
+        // Se passa -1 su questo parametro vuol recuperare tutte le strutture di livello 1
+        if (idl1 == DEFAULT_ID) {
+            level = (byte) 1;
+            tableFrom = "struttura_liv1";
+            tableWhere = DEFAULT_ID + " = " + idl1 ;
+        // Altrimenti, se passa un id, vuol recuperare tutte le strutture di livello 2 che afferiscono alla struttura di livello 1 di tale id
+        } else if (idl1 > NOTHING) {
+            level = (byte) 2;
+            tableFrom = "struttura_liv2";
+            tableWhere = "id_struttura_liv1 = " + idl1;
+        }
+        // Liv 2
+        if (idl2 == DEFAULT_ID) {       // Tutte le Liv 2
+            level = (byte) 2;
+            tableFrom = "struttura_liv2";
+            tableWhere = DEFAULT_ID + " = " + idl2 ;
+        } else if (idl2 > NOTHING) {    // Solo le Liv 3 afferenti a una specifica Liv 2
+            level = (byte) 3;
+            tableFrom = "struttura_liv3";
+            tableWhere = "id_struttura_liv2 = " + idl2;
+        }
+        // Liv 3
+        if (idl3 == DEFAULT_ID) {       // Tutte le Liv 3
+            level = (byte) 3;
+            tableFrom = "struttura_liv3";
+            tableWhere = DEFAULT_ID + " = " + idl3 ;
+        } else if (idl3 > NOTHING) {    // Solo le Liv 4 afferenti a una specifica Liv 3
+            level = (byte) 4;
+            tableFrom = "struttura_liv4";
+            tableWhere = "id_struttura_liv3 = " + idl3;
+        }
+        // Liv 4
+        if (idl4 == DEFAULT_ID) {       // Tutte le Liv 4
+            level = (byte) 4;
+            tableFrom = "struttura_liv4";
+            tableWhere = DEFAULT_ID + " = " + idl4 ;
+        }
+        final String GET_STRUCTURE_BY_STRUCTURE =
+                "SELECT " +
+                "       D.id                 AS \"id\"" +
+                "   ,   D.nome               AS \"nome\"" +
+                "   ,   D.codice             AS \"informativa\"" +
+                "   ,   D.ordinale           AS \"ordinale\"" +
+                "   ,   D.prefisso           AS \"prefisso\"" +
+                "   ,   D.acronimo           AS \"acronimo\"" +
+                "   ,   D.indirizzo_sede     AS \"indirizzo\"" +
+                "   ," + level + "::SMALLINT AS \"livello\"" +
+                "   FROM " + tableFrom + " D" +
+                "   WHERE D.id_rilevazione = " + idR +
+                "       AND " + tableWhere +
+                "       AND id_stato = 1" + // Prende solo le strutture attive
+                "   ORDER BY D.nome, D.ordinale";
+        return GET_STRUCTURE_BY_STRUCTURE;
     }
 
+
+    /* (non-Javadoc)
+     * @see it.rol.Query#getQueryStructure(int, byte)
+     */
+    @SuppressWarnings("javadoc")
     @Override
-    public String getQueryStructure(int idS, byte level) {
-        // TODO Auto-generated method stub
-        return null;
+    public String getQueryStructure(int id,
+                                    byte level) {
+        String table = null;
+        switch(level) {
+            case 1:
+                table = "struttura_liv1";
+                break;
+            case 2:
+                table = "struttura_liv2";
+                break;
+            case 3:
+                table = "struttura_liv3";
+                break;
+            case 4:
+                table = "struttura_liv4";
+                break;
+        }
+        final String GET_STRUCTURE_BY_ID =
+                "SELECT " +
+                "       D.id        AS \"id\"" +
+                "   ,   D.nome      AS \"nome\"" +
+                "   ,   D.codice    AS \"informativa\"" +
+                "   ,   D.prefisso  AS \"prefisso\"" +
+                "   ,   D.acronimo  AS \"acronimo\"" +
+                "   ,   D.ordinale  AS \"ordinale\"" +
+                "   ," + level +  " AS \"livello\"" + 
+                "   FROM " + table + " D" +
+                "   WHERE D.id = " + id +
+                "   ORDER BY D.ordinale";
+        return GET_STRUCTURE_BY_ID;
     }
+
 
     @Override
     public String getQueryStructureBySurvey(int idR, int idl4, int idl3, int idl2, int idl1) {
